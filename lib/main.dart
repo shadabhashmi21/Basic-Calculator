f3e1cf1c760baaca512e20ee45cea58242b8886f
Shadab Hashmi <er.shadabhashmi@gmail.com>
2020-02-29 21:20:04 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(BasicCalculator());
 
-class BasicCalculator extends StatelessWidget {
+class BasicCalculator extends StatefulWidget {
+  @override
+  _BasicCalculatorState createState() => _BasicCalculatorState();
+}
+
+class _BasicCalculatorState extends State<BasicCalculator> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text("Basic Calculator",),
+          title: Text("Basic Calculator"),
           backgroundColor: Colors.redAccent,
         ),
         backgroundColor: Colors.white70,
+        body: new Container(
+        ),
       ),
     );
   }
