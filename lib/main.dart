fbf0d9501e1bf56bd09999f20239dbf3e2137bbe
Shadab Hashmi <er.shadabhashmi@gmail.com>
2020-03-01 22:01:40 +0530

    second commit/UI almost completed

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,77 @@ class _BasicCalculatorState extends State<BasicCalculator> {
           backgroundColor: Colors.redAccent,
         ),
         backgroundColor: Colors.white70,
-        body: new Container(
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints.expand(
+                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 100.0,
+                ),
+                alignment: Alignment.bottomRight,
+                color: Colors.white,
+                child: Text("0",
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.black
+                ),
+                  textAlign: TextAlign.right,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _button("9"),
+                  _button("8"),
+                  _button("7"),
+                  _button("+"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _button("6"),
+                  _button("5"),
+                  _button("4"),
+                  _button("-"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _button("3"),
+                  _button("2"),
+                  _button("1"),
+                  _button("*"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _button("C", /*_clear*/),
+                  _button("0", /*_zero*/),
+                  _button("=", /*_disp*/),
+                  _button("/", /*_div*/),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+  Widget _button(String number, /*Function() f*/){
+    return MaterialButton(
+      height: 100.0,
+      child: Text(number,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
+      textColor: Colors.black,
+      color: Colors.grey[100],
+      //onPressed: f,
+    );
+  }
 }
 
-void calculate() {
-
+class _div {
 }
