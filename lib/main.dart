37c6d4b6e2b3a1c92ba12b5567f4af3d721bef12
Shadab Hashmi <er.shadabhashmi@gmail.com>
2020-03-05 13:06:02 +0530

    Almost Done

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,47 @@ class BasicCalculator extends StatefulWidget {
 }
 
 class _BasicCalculatorState extends State<BasicCalculator> {
+  String output = "0";
+  String _output = "0";
+  double n1 = 0;
+  double n2 = 0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if(buttonText == "C"){
+      _output = "0";
+      n1 = 0;
+      n2 = 0;
+      operand = "";
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/"){
+      n1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == "=") {
+      n2 = double.parse(output);
+      if(operand == "+"){
+        _output = (n1 + n2).toString();
+      }
+      if(operand == "-"){
+        _output = (n1 - n2).toString();
+      }
+      if(operand == "*"){
+        _output = (n1 * n2).toString();
+      }
+      if(operand == "/"){
+        _output = (n1 / n2).toString();
+      }
+      n1 = 0;
+      n2 = 0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+    print(_output);
+    setState(() {
+      output = double.parse(_output).toString();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,7 +70,7 @@ class _BasicCalculatorState extends State<BasicCalculator> {
                 ),
                 alignment: Alignment.bottomRight,
                 color: Colors.white,
-                child: Text("0",
+                child: Text(output,
                 style: TextStyle(
                   fontSize: 50,
                   color: Colors.black
@@ -79,17 +120,14 @@ class _BasicCalculatorState extends State<BasicCalculator> {
       ),
     );
   }
-  Widget _button(String number, /*Function() f*/){
+  Widget _button(String buttonText, /*Function() f*/){
     return MaterialButton(
       height: 100.0,
-      child: Text(number,
+      child: Text(buttonText,
       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
       textColor: Colors.black,
       color: Colors.grey[100],
-      //onPressed: f,
+      onPressed: () => buttonPressed(buttonText),
     );
   }
-}
-
-class _div {
 }
